da74d19cdf99e25e7a85850b2dbd3d5e53a73354
alexs <alexs@AMBIBLNDV>
2026-03-03 21:27:48 +0200

    Add project files.

---
+++ Laborator1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeworkAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numărul de ore lucrate: ");
+            int oreLucrate = int.Parse(Console.ReadLine());
+            Console.Write("Introduceți tariful pe ora: ");
+            double tarifPeOra = double.Parse(Console.ReadLine());
+            double salariu = (double)oreLucrate * tarifPeOra;
+            Console.WriteLine($"\nSalariul calculat este: {salariu} lei");
+            if (salariu > 3000)
+            {
+                Console.WriteLine("Salariu mare");
+            }
+            else
+            {
+                Console.WriteLine("Ati lucrat prea putine ore pentru a avea un salariu mare!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
